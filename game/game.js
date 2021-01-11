b53b3fdbe87e46c9fdc93a9c9afdd860813711af
aziz-sassi <sassiaziz50@gmail.com>
2021-01-11 23:25:04 +0100

    final push

--- game/game.js
+++ game/game.js
@@ -1,4 +1,9 @@
-$('.showen').hide()
+var score = $('#score');
+var lifess = 5;
+$('.showwin').hide();
+$('.showen').hide();
+$('.showlose').hide();
+
 $('#hider').on('click', function (){
 	$('.hiden').hide(1000);
 	$('.showen').show(1000);
@@ -8,4 +13,39 @@ $('#hider').on('click', function (){
 })
 function insrt(num) {
 	$('.display').val($('.display').val()+num);}
+	function C() {
+	$('.display').val('');
+}
+	
+
+var playersGuess;
+ var winningNumber;
+
+function random(){
+	lifess = lifess-1
+	score.text(parseInt(score.text()) - 1)
+    winningNumber = Math.floor(Math.random() * 10);
+   
+    playersGuess = parseInt($('#playersGuess').val());
+    if (winningNumber===playersGuess) {
+    	$('.main').hide();
+    	$('.showwin').show(1000)
+    }
+   else if (lifess<=0) {
+   	$('.main').hide();
+   	$('.showlose').show(1000)
+
+   }
+}
+
+
+
+
+
+
+    
+   
+
+
+
 
